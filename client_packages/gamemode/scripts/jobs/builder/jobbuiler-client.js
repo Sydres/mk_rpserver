cfa631f06f0c18464c944a2d55df8901d8e9a5af
ITBoomBKStudio <bohdan1403@gmail.com>
2019-07-19 23:13:14 +0300

    Blip

--- client_packages/gamemode/scripts/jobs/builder/jobbuiler-client.js
+++ client_packages/gamemode/scripts/jobs/builder/jobbuiler-client.js
@@ -80,6 +80,7 @@ mp.events.add('createJobNeedBuilderMarkBlip', (posx, posy, posz) => {
 		JobBuilder.marker = mp.markers.new(1, new mp.Vector3(posx, posy, posz - 1.4), 4, { visible: true, dimension: 0, color: [255, 0, 0, 110] });
 		JobBuilder.blip = mp.blips.new(1, new mp.Vector3(posx, posy, posz - 1.4 ), { alpha: 255, color: 1 });
 		JobBuilder.blip.setRoute(true);
+    JobBuilder.blip.rotation = 180;
 		JobBuilder.blip.setRouteColour(1);
 		JobBuilder.colshape = mp.colshapes.newSphere(posx, posy, posz, 2);
 	} catch (err) {
