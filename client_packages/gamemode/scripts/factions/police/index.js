f21b036cc68c360f6b354b23594522cbf1d41e40
ITBoomBKStudio <bohdan1403@gmail.com>
2019-07-22 02:54:29 +0300

    police ui

--- client_packages/gamemode/scripts/factions/police/index.js
+++ client_packages/gamemode/scripts/factions/police/index.js
@@ -21,187 +21,188 @@ const Natives = {
     DOES_BLIP_EXIST: '0xA6DB27D19ECBB7DA',
 };
 
-mp.events.add('setTaxiJobStatus', (status) => { keyDownE = status; });
-mp.events.add('getTaxiJobStatus', (status) => {
-  if (status !== "cancel") {
-    mp.events.call("prompt.show", `Нажмите <span>Е</span> для взаимодействия`);
-    keyDownE = status;
-  } else {
-    keyDownE = null;
+mp.events.add('update.police.stats', (money) => {
+ PoliceUI.orders_today++;
+ PoliceUI.earnmoney_today += money;
+ if (CEF) CEF.execute(`app.updatepoliceStats(${PoliceUI.orders_today}, ${PoliceUI.earnmoney_today});`);
+});
+
+mp.events.add('clear.police.filtration', (type) => {
+  PoliceUI.blacklist = [];
+  mp.game.ui.notifications.showWithPicture('Информация', 'LSPD', 'Вы ~y~сбросили ~w~фильтрацию заказов.', 'CHAR_police');
+});
+
+mp.events.add('add.police.filtration', (name) => {
+  if (!PoliceUI.blacklist.includes(name)) {
+    PoliceUI.blacklist.push(name);
+    mp.game.ui.notifications.showWithPicture('Информация', 'LSPD',  '~y~' + name + ' ~w~добавлен в фильтрацию и не будет отображаться.', 'CHAR_police');
   }
 });
-mp.events.add('update.taxi.stats', (money) => {
- JobTaxi.orders_today++;
- JobTaxi.earnmoney_today += money;
- if (CEF) CEF.execute(`app.updateTaxiStats(${JobTaxi.orders_today}, ${JobTaxi.earnmoney_today});`);
-});
-mp.events.add('clear.taxi.filtration', (type) => {
-  JobTaxi.blacklist = [];
-  mp.game.ui.notifications.showWithPicture('Информация', 'Downtown Cab Co.', 'Вы ~y~сбросили ~w~фильтрацию заказов.', 'CHAR_TAXI');
-});
-mp.events.add('add.taxi.filtration', (name) => {
-  if (!JobTaxi.blacklist.includes(name)) {
-    JobTaxi.blacklist.push(name);
-    mp.game.ui.notifications.showWithPicture('Информация', 'Downtown Cab Co.',  '~y~' + name + ' ~w~добавлен в фильтрацию и не будет отображаться.', 'CHAR_TAXI');
-  }
-});
-mp.events.add('show.taxi.menu', (name) => {
+
+mp.events.add('show.police.menu', (name) => {
   if (!CEF) {
-    CEF = mp.browsers.new("package://gamemode/scripts/jobs/taxi/ui/panel/index.html");
+    CEF = mp.browsers.new("package://gamemode/scripts/jobs/police/ui/panel/index.html");
     CEF.execute(`app.setNameForPassword('${name}')`);
   }
 });
-mp.events.add('control.taxi.menu', (type, del) => {
+mp.events.add('control.police.menu', (type, del) => {
   if (type === true) {
     if (CEF) CEF.execute("document.getElementById('app').style.display = 'block';");
-    if (del === true) JobTaxi.time = -1;
+    if (del === true) PoliceUI.time = -1;
   } else {
     if (CEF) CEF.execute("document.getElementById('app').style.display = 'none';");
-    if (del === false) JobTaxi.time = 10; // 10 * 6 = 60 секунд
+    if (del === false) PoliceUI.time = 10; // 10 * 6 = 60 секунд
   }
 });
-mp.events.add('cant.finish.taxi.job', () => {
-  mp.game.ui.notifications.showWithPicture('Информация', 'Downtown Cab Co.', 'Вы ~y~не завершили ~w~вызов.', 'CHAR_TAXI');
+
+mp.events.add('cant.finish.police.job', () => {
+  mp.game.ui.notifications.showWithPicture('Информация', 'LSPD', 'Вы ~y~не завершили ~w~вызов.', 'CHAR_police');
 });
-mp.events.add('remove.taxi.order', (name) => {
-  if (CEF) CEF.execute(`app.removeFromTaxi('${name}')`);
+
+mp.events.add('remove.police.order', (name) => {
+  //if (CEF) CEF.execute(`app.removeFrompolice('${name}')`);
 });
-mp.events.add('take.taxi.order', (name) => {
-  mp.events.callRemote('take.taxi.order', name);
+
+mp.events.add('take.police.order', (name) => {
+  mp.events.callRemote('take.police.order', name);
 });
-mp.events.add('accept.taxi.order', (pos) => {
-    JobTaxi.blip = mp.blips.new(280, pos, { alpha: 255, color: 5, scale: 0.9, name: "Клиент" });
-    JobTaxi.blip.setRoute(true);
-    JobTaxi.blip.setRouteColour(5);
-    JobTaxi.colshape = mp.colshapes.newSphere(pos.x, pos.y, pos.z, 6);
-    JobTaxi.marker = mp.markers.new(1, new mp.Vector3(pos.x, pos.y, pos.z - 1.5), 12, { visible: true, dimension: 0, color: [255, 0, 0, 40] });
+
+mp.events.add('accept.police.order', (pos) => {
+    PoliceUI.blip = mp.blips.new(280, pos, { alpha: 255, color: 5, scale: 0.9, name: "Клиент" });
+    PoliceUI.blip.setRoute(true);
+    PoliceUI.blip.setRouteColour(5);
+    PoliceUI.colshape = mp.colshapes.newSphere(pos.x, pos.y, pos.z, 6);
+    PoliceUI.marker = mp.markers.new(1, new mp.Vector3(pos.x, pos.y, pos.z - 1.5), 12, { visible: true, dimension: 0, color: [255, 0, 0, 40] });
     if (CEF) {
        CEF.execute(`app.setselect();`);
        CEF.execute(`app.startTime(${0}, "${getZoneName(pos)}");`);
     }
 });
-mp.events.add('destroy.taxi.colshape', () => {
-  if (JobTaxi.blip !== undefined) {
-    JobTaxi.blip.setRoute(false);
-    JobTaxi.blip.destroy();
+
+mp.events.add('destroy.police.colshape', () => {
+  if (PoliceUI.blip !== undefined) {
+    PoliceUI.blip.setRoute(false);
+    PoliceUI.blip.destroy();
   }
-  if (JobTaxi.colshape_go !== undefined) JobTaxi.colshape_go.destroy();
-  if (JobTaxi.marker !== undefined) JobTaxi.marker.destroy();
-  if (JobTaxi.colshape !== undefined) JobTaxi.colshape.destroy();
-  delete JobTaxi.blip, delete JobTaxi.colshape, delete JobTaxi.colshape_go, delete JobTaxi.marker;
+  if (PoliceUI.colshape_go !== undefined) PoliceUI.colshape_go.destroy();
+  if (PoliceUI.marker !== undefined) PoliceUI.marker.destroy();
+  if (PoliceUI.colshape !== undefined) PoliceUI.colshape.destroy();
+  delete PoliceUI.blip, delete PoliceUI.colshape, delete PoliceUI.colshape_go, delete PoliceUI.marker;
 });
 mp.keys.bind(0x60, false, function () { // Numpad 0
-  if (mp.players.local.vehicle && JobTaxi.interval !== undefined) mp.events.callRemote("key.taxi.down.numpad_zero");
+  if (mp.players.local.vehicle && PoliceUI.interval !== undefined) mp.events.callRemote("key.police.down.numpad_zero");
 });
 mp.events.add('playerEnterColshape', (shape) => {
-    if (shape === JobTaxi.colshape && mp.players.local.vehicle) mp.events.callRemote("enter.taxi.colshape");
+    if (shape === PoliceUI.colshape && mp.players.local.vehicle) mp.events.callRemote("enter.police.colshape");
 });
 mp.events.add('playerExitColshape', (shape) => {
-    if (shape === JobTaxi.colshape_go) mp.events.callRemote("cancel.taxi.player");
+    if (shape === PoliceUI.colshape_go) mp.events.callRemote("cancel.police.player");
 });
-mp.events.add('displace.taxi.menu', () => {
-  if (JobTaxi.type === true) {
-     if (CEF) CEF.execute('app.taxiPage = false;');
-     JobTaxi.type = false;
+mp.events.add('displace.police.menu', () => {
+  if (PoliceUI.type === true) {
+     if (CEF) CEF.execute('app.policePage = false;');
+     PoliceUI.type = false;
   } else {
-     if (CEF) CEF.execute('app.taxiPage = true;');
-     JobTaxi.type = true;
+     if (CEF) CEF.execute('app.policePage = true;');
+     PoliceUI.type = true;
   }
 });
-mp.events.add('delete.taxi.player.colshape', () => {
-  if (JobTaxi.colshape_go !== undefined) JobTaxi.colshape_go.destroy();
-  if (JobTaxi.marker !== undefined) JobTaxi.marker.destroy();
-  delete JobTaxi.colshape_go, delete JobTaxi.marker;
+mp.events.add('delete.police.player.colshape', () => {
+  if (PoliceUI.colshape_go !== undefined) PoliceUI.colshape_go.destroy();
+  if (PoliceUI.marker !== undefined) PoliceUI.marker.destroy();
+  delete PoliceUI.colshape_go, delete PoliceUI.marker;
 });
-mp.events.add('create.taxi.player.colshape', () => {
-  JobTaxi.marker = mp.markers.new(1, new mp.Vector3(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z - 1.5), 12, { visible: true, dimension: 0, color: [255, 0, 0, 40] });
-  JobTaxi.colshape_go = mp.colshapes.newSphere(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z, 6);
+mp.events.add('create.police.player.colshape', () => {
+  PoliceUI.marker = mp.markers.new(1, new mp.Vector3(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z - 1.5), 12, { visible: true, dimension: 0, color: [255, 0, 0, 40] });
+  PoliceUI.colshape_go = mp.colshapes.newSphere(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z, 6);
 });
-mp.events.add('update.taxi.interval', (type) => {
+
+mp.keys.bind(0x60, false, function () { // Numpad 0
+  if (mp.players.local.vehicle && PoliceUI.interval !== undefined) mp.events.callRemote("key.police.down.numpad_zero");
+});
+
+mp.events.add('displace.police.menu', () => {
+  if (PoliceUI.type === true) {
+     if (CEF) CEF.execute('app.policePage = false;');
+     PoliceUI.type = false;
+  } else {
+     if (CEF) CEF.execute('app.policePage = true;');
+     PoliceUI.type = true;
+  }
+});
+mp.events.add('update.police.interval', (type) => {
   if (type) {
-    JobTaxi.interval = setInterval(() => {
-       mp.events.callRemote("update.taxi.orders");
-       if (JobTaxi.time > -1) JobTaxi.time--;
-       if (JobTaxi.time === 0) {
-         mp.events.callRemote("end.taxi.day");
+    PoliceUI.interval = setInterval(() => {
+       mp.events.callRemote("update.police.orders");
+       if (PoliceUI.time > -1) PoliceUI.time--;
+       if (PoliceUI.time === 0) {
+         mp.events.callRemote("end.police.day");
          return;
        }
     }, 6000);
   } else {
-    clearInterval(JobTaxi.interval);
-    delete JobTaxi.interval;
+    clearInterval(PoliceUI.interval);
+    delete PoliceUI.interval;
   }
 });
-mp.events.add('update.taxi.orders', (arr) => {
+mp.events.add('update.police.orders', (arr) => {
   let orders = JSON.parse(arr);
   orders.forEach(function(order) {
-    if (!JobTaxi.blacklist.includes(order.client_name) && order.taxist_name === undefined) {
-      if (CEF) CEF.execute(`app.addToTaxi('${order.client_name}', '${Math.trunc(mp.game.gameplay.getDistanceBetweenCoords(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z, order.start_position.x, order.start_position.y, order.start_position.z, true))}');`);
+    if (!PoliceUI.blacklist.includes(order.client_name) && order.policest_name === undefined) {
+      if (CEF) CEF.execute(`app.addTopolice('${order.client_name}', '${Math.trunc(mp.game.gameplay.getDistanceBetweenCoords(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z, order.start_position.x, order.start_position.y, order.start_position.z, true))}');`);
     }
   });
 });
-mp.events.add('get.taxi.waypoint.driver', (name, money, pos) => {
-     if (JobTaxi.blip !== undefined) {
-       JobTaxi.blip.setRoute(false);
-       JobTaxi.blip.destroy();
+mp.events.add('get.police.waypoint.driver', (name, money, pos) => {
+     if (PoliceUI.blip !== undefined) {
+       PoliceUI.blip.setRoute(false);
+       PoliceUI.blip.destroy();
      }
-     if (JobTaxi.colshape !== undefined) JobTaxi.colshape.destroy();
-     if (JobTaxi.marker !== undefined) JobTaxi.marker.destroy();
-     delete JobTaxi.blip, delete JobTaxi.colshape, delete JobTaxi.marker;
+     if (PoliceUI.colshape !== undefined) PoliceUI.colshape.destroy();
+     if (PoliceUI.marker !== undefined) PoliceUI.marker.destroy();
+     delete PoliceUI.blip, delete PoliceUI.colshape, delete PoliceUI.marker;
 
-     JobTaxi.blip = mp.blips.new(1, pos, { alpha: 255, color: 5, scale: 0.9, name: "Пункт назначения" });
-     JobTaxi.blip.setRoute(true);
-     JobTaxi.blip.setRouteColour(5);
+     PoliceUI.blip = mp.blips.new(1, pos, { alpha: 255, color: 5, scale: 0.9, name: "Пункт назначения" });
+     PoliceUI.blip.setRoute(true);
+     PoliceUI.blip.setRouteColour(5);
 
      if (CEF) CEF.execute(`app.setInfo("${name}", ${money}, "${getZoneName(pos)}");`);
 });
-mp.events.add('get.taxi.waypoint', (target) => {
+mp.events.add('get.police.waypoint', (target) => {
    let pos = getWaypointPosition();
    if (pos === undefined) {
-     mp.game.ui.notifications.showWithPicture('Информация', 'Downtown Cab Co.', 'Вы ~y~не установили ~w~метку на карте.', 'CHAR_TAXI');
+     mp.game.ui.notifications.showWithPicture('Информация', 'LSPD', 'Вы ~y~не установили ~w~метку на карте.', 'CHAR_police');
      return;
    }
 
    let dist = mp.game.gameplay.getDistanceBetweenCoords(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z, pos.x, pos.y, 0, false);
-   mp.events.callRemote("set.taxi.waypoint", target, dist, pos.x, pos.y, mp.players.local.position.z);
+   mp.events.callRemote("set.police.waypoint", target, dist, pos.x, pos.y, mp.players.local.position.z);
 });
-mp.events.add('cancel.taxi.order', (type) => {
-  if (JobTaxi.blip !== undefined) {
-    JobTaxi.blip.setRoute(false);
-    JobTaxi.blip.destroy();
+
+mp.events.add('cancel.police.order', (type) => {
+  if (PoliceUI.blip !== undefined) {
+    PoliceUI.blip.setRoute(false);
+    PoliceUI.blip.destroy();
   }
-  if (JobTaxi.colshape !== undefined) JobTaxi.colshape.destroy();
-  if (JobTaxi.marker !== undefined) JobTaxi.marker.destroy();
-  if (JobTaxi.interval !== undefined && type === true) clearInterval(JobTaxi.interval);
-  JobTaxi.type = true;
+  if (PoliceUI.colshape !== undefined) PoliceUI.colshape.destroy();
+  if (PoliceUI.marker !== undefined) PoliceUI.marker.destroy();
+  if (PoliceUI.interval !== undefined && type === true) clearInterval(PoliceUI.interval);
+  PoliceUI.type = true;
   if (type === true) {
-    JobTaxi.blacklist = [];
-    JobTaxi.time = -1;
+    PoliceUI.blacklist = [];
+    PoliceUI.time = -1;
   }
-  delete JobTaxi.blip, delete JobTaxi.colshape, delete JobTaxi.marker, delete JobTaxi.interval;
+  delete PoliceUI.blip, delete PoliceUI.colshape, delete PoliceUI.marker, delete PoliceUI.interval;
 });
-mp.events.add('close.taxi.control', () => {
+mp.events.add('close.police.control', () => {
   if (CEF) CEF.execute(`app.deleteselect()`);
 });
-mp.events.add('close.taxi.menu', () => {
+mp.events.add('close.police.menu', () => {
   if (CEF) {
     CEF.destroy();
     CEF = null;
     // При подключение в браузер удалять все бесполезное с меню.
   }
-});
-mp.keys.bind(0x45, false, function () { // E key
-	if (keyDownE !== undefined) {
-		if (mp.game.gameplay.getDistanceBetweenCoords(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z, 894.892, -179.171, 74.700, true) < 4){
-      mp.gui.cursor.show(true, true);
-      if (keyDownE === true) {
-          mp.events.call("choiceMenu.show", "accept_job_taxi", {name: "уволиться из Таксопарка?"});
-      } else {
-					mp.events.call("choiceMenu.show", "accept_job_taxi", {name: "устроиться в Таксопарк?"});
-      }
-		}
-	}
-  if (mp.players.local.vehicle) mp.events.callRemote("key.taxi.down.e");
 });
 
 // Functions
