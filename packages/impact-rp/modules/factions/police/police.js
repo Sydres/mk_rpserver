d5de1b590616cc97dc73f31071885fd9e7de10c7
ComeGetSome0 <baklajantv@gmail.com>
2019-08-05 23:08:26 +0300

    police call

--- packages/impact-rp/modules/factions/police/police.js
+++ packages/impact-rp/modules/factions/police/police.js
@@ -69,3 +69,35 @@ function deleteOrderForofficer(name) {
     }
 }
 module.exports.recallPolice = recallPolice;
+"police.addCall": (player, text) => {
+    // TODO: Проверка на наличие мобилы.
+
+    mp.players.forEach((rec) => {
+        if (mp.factions.isPoliceFaction(rec.faction)) {
+            rec.call(`tablet.police.addCall`, [{
+                id: player.id,
+                name: player.name,
+                pos: player.position,
+                message: text
+            }]);
+        }
+    });
+    player.policeCallTime = new Date().getTime();
+    player.utils.success(`Вызов отправлен!`);
+},
+"police.acceptCall": (player, playerId) => {
+    if (!mp.factions.isPoliceFaction(player.faction)) return player.utils.error(`Вы не сотрудник порядка!`);
+    var rec = mp.players.at(playerId);
+    if (!rec) return player.utils.error(`Гражданин не найден!`);
+    if (!rec.policeCallTime) return player.utils.error(`Вызов принят/отклонен другим полицейским!`);
+
+    mp.players.forEach((rec) => {
+        if (mp.factions.isPoliceFaction(rec.faction)) {
+            rec.call(`tablet.police.removeCall`, [playerId]);
+        }
+    });
+
+    delete rec.policeCallTime;
+    player.utils.success(`Вызов принят!`);
+    rec.utils.success(`${player.name} принял Ваш вызов!`);
+},
