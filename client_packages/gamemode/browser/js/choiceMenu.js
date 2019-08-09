009ef84baa0b77e2349b1321c1c343cd95593afc
IronRagnar <anton.mltsv@gmail.com>
2019-08-09 12:22:52 +0300

    enter

--- client_packages/gamemode/browser/js/choiceMenu.js
+++ client_packages/gamemode/browser/js/choiceMenu.js
@@ -87,11 +87,12 @@ $(document).ready(() => {
             },
         },
 
+
         "accept_job_grapecollector": {
             text: "Вы хотите начать работу <Работа>?",
             on: (values) => {
                 menus["accept_job_grapecollector"].text = `Вы хотите ${values.name}`;
-            }, 
+            },
             yes: () => {
                 mp.trigger("events.callRemote", "job.grapecollector.agree");
             },
